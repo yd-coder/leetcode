26052580ffb8d7ca4fa5cb63d4cadc7b5d5974e0
屿东 <2395183536@qq.com>
2023-06-24 21:41:42 +0800

    🐞 fix: 修改部署配置

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  /**
+   * 使用next export命令导出Next.js应用程序时，存在与默认图片优化加载器不兼容的问题。
+   * Next.js 默认的图片优化方案需要通过 Image Optimization API 在服务器端运行，而导出的静态站点无法访问该 API。
+   * 为了解决这个问题，你可以将 images.unoptimized 配置为 true，这样 Next.js 将不会对图片进行优化。
+   */
   images: {
-    unoptimized: true
+    unoptimized: true 
   }
 }
 

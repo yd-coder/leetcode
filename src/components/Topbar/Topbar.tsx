5bd94edc7130e69373013acea876857274f8e9b6
屿东 <2395183536@qq.com>
2023-06-24 22:33:33 +0800

    🐞 fix: 修复图片路径问题

--- src/components/Topbar/Topbar.tsx
+++ src/components/Topbar/Topbar.tsx
@@ -42,7 +42,7 @@ const Topbar: React.FC<TopbarProps> = ({problemPage}) => {
         <nav className='relative flex h-[50px] w-full shrink-0 items-center px-10 bg-dark-layer-1 text-dark-gray-7'>
             <div className={`flex w-full items-center justify-between ${!problemPage ? "max-w-[1200px] mx-auto" : ""}`}>
                 <Link href='/' className='h-[22px] min-w-[59px] flex-1'>
-                    <Image src='/logo.svg' alt='Logo' width={59} height={22}/>
+                    <Image src='./logo.svg' alt='Logo' width={59} height={22}/>
                 </Link>
 
                 {problemPage && (
@@ -79,7 +79,7 @@ const Topbar: React.FC<TopbarProps> = ({problemPage}) => {
                     </div>
                     <Timer/>
                     <div className='cursor-pointer group relative min-w-[30px] min-h-[30px]'>
-                        <Image src='/avatar.png' alt='Avatar' width={30} height={30} className='rounded-full'/>
+                        <Image src='./avatar.png' alt='Avatar' width={30} height={30} className='rounded-full'/>
                         <div
                             className='absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg
 								z-40 group-hover:scale-100 scale-0 

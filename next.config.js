cf4735a96bfdc3a323384cc056803bf12142a298
屿东 <2395183536@qq.com>
2023-06-24 22:20:42 +0800

    🐞 fix: 修改部署配置

--- next.config.js
+++ next.config.js
@@ -8,7 +8,13 @@ const nextConfig = {
    */
   images: {
     unoptimized: true 
-  }
+  },
+  // 启用客户端路由，以使 Next.js 应用程序在 GitHub Pages 上正常工作
+  generateBuildId: async () => {
+    return 'build'
+  },
+  assetPrefix: './',
+  basePath: ''
 }
 
 module.exports = nextConfig
